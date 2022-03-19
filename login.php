0361910b90c3e15abad76d91344125e779b26baf
Nitin Ravi <nitinravi18@gmail.com>
2022-03-19 14:27:12 +0530

    login verification done

---
+++ login.php
@@ -0,0 +1,27 @@
+<?php
+include "config.php";
+
+if(isset($_POST['submit'])){
+
+    $username = mysqli_real_escape_string($con,$_POST['username']);
+    $password = mysqli_real_escape_string($con,$_POST['password']);
+
+    if ($username != "" && $password != ""){
+
+        $sql_query = "select count(*) as cntUser from register where uname1='".$username."' and upswd1='".$password."'";
+        $result = mysqli_query($con,$sql_query);
+        $row = mysqli_fetch_array($result);
+
+        $count = $row['cntUser'];
+
+        if($count > 0){
+            $_SESSION['username'] = $username;
+            header('Location: index.php');
+        }else{
+            echo "Invalid username and password";
+        }
+
+    }
+
+}
+?>
